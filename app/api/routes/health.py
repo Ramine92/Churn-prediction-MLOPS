961713393af3eabde9a5aba84785eba8dbe45fa6
Graia Ramin <142338131+Ramine92@users.noreply.github.com>
2026-02-24 02:05:07 +0100

    added modelname to health endpoint

--- app/api/routes/health.py
+++ app/api/routes/health.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-
+from app.core.confing import MODEL_NAME
 router = APIRouter()
 
 @router.get("/health")
 def health():
-    return {"status":"ok"}
+    return {"status":"ok","model_name":MODEL_NAME}
